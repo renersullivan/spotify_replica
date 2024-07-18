552b6d9ceb7dcd9b327bb55554af44e2bf8533cf
Felipe Ferreira Cabo <felipeferreirah@gmail.com>
2024-07-18 17:42:15 -0300

    Subir menu responsivo do spotify

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+   
+  abriuMenu:boolean = false
+
+  constructor(@Inject(DOCUMENT) private document: Document){
+    
+  }
+
+  public actionAbrirMenu():void {
+    this.abriuMenu = !this.abriuMenu;
+    this.document.body.querySelector(".menu-mobile-overflow")?.classList.toggle("abriu")
+  }
+
 
 }
+function ViewChild(arg0: string): (target: HomeComponent, propertyKey: "input") => void {
+  throw new Error('Function not implemented.');
+}
+
