6f92da21c4afe02b974ef394f839405b9d20130f
renersullivan <renersullivan1@gmail.com>
2024-08-17 22:10:08 -0300

    criando page musicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MusicasComponent } from './pages/musicas/musicas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    MusicasComponent
   ],
   imports: [
     BrowserModule,
